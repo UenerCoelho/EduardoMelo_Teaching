a1cdbfa265c3769eeb7db8e0474a7546d773dba7
Uener <uenercoelho2@hotmail.com>
2022-04-08 22:58:31 -0400

    Challenge 3

--- Desafios/desafio_02.js
+++ Desafios/desafio_02.js
@@ -1,67 +1,67 @@
-// // Crie uma função que receba dois argumentos e retorne a soma dos mesmos.
-// var num1 = 2
-// var num2 = 3
-// function soma() {
-//   return num1 + num2
-// }
-// console.log(soma())
-// // Declare uma variável que receba a invocação da função criada acima, passando dois números quaisquer por argumento, e somando `5` ao resultado retornado da função.
-// console.log(soma((num1 = 10), (num2 = 10)) + 5)
-// // Qual o valor atualizado dessa variável?
-// 25
+// Crie uma função que receba dois argumentos e retorne a soma dos mesmos.
+var num1 = 2
+var num2 = 3
+function soma() {
+  return num1 + num2
+}
+console.log(soma())
+// Declare uma variável que receba a invocação da função criada acima, passando dois números quaisquer por argumento, e somando `5` ao resultado retornado da função.
+console.log(soma((num1 = 10), (num2 = 10)) + 5)
+// Qual o valor atualizado dessa variável?
+25
 
-// // Declare uma nova variável, sem valor.
-// var num
-// /*
-// Crie uma função que adicione um valor à variável criada acima, e retorne a string:
-//     O valor da variável agora é 10.
-//     Onde 10 é o novo valor da variável.
-// */
-// function value(valor) {
-//   num = valor;
-//   var text = 'O valor da variável agora é: ' + num;
-//   return text;
-// }
-// console.log(value(10))
+// Declare uma nova variável, sem valor.
+var num
+/*
+Crie uma função que adicione um valor à variável criada acima, e retorne a string:
+    O valor da variável agora é 10.
+    Onde 10 é o novo valor da variável.
+*/
+function value(valor) {
+  num = valor;
+  var text = 'O valor da variável agora é: ' + num;
+  return text;
+}
+console.log(value(10))
 
-// // Invoque a função criada acima.
-// // value()
+// Invoque a função criada acima.
+// value()
 
-// // Qual o retorno da função? (Use comentários de bloco).
-// //'10'
+// Qual o retorno da função? (Use comentários de bloco).
+//'10'
 
-// /*
-// Crie uma função com as seguintes características:
-// 1. A função deve receber 3 argumentos;
-// 2. Se qualquer um dos três argumentos não estiverem preenchidos, a função deve retornar a string:
-//     Preencha todos os valores corretamente!
-// 3. O retorno da função deve ser a multiplicação dos 3 argumentos, somando `2` ao resultado da multiplicação.
-// */
-// // var a = 2
-// // var b = 3
-// // var c = 4
-// function multiplication(a, b, c) {
-//   if ((!a || !b || !c)) {
-//     var text = 'Preencha todos os valores corretamente!';
-//     return text;
-//   } else {
-//     return a * b * c + 2
-//   }
-// }
-// console.log(multiplication())
-// console.log(multiplication(1,2,3))
+/*
+Crie uma função com as seguintes características:
+1. A função deve receber 3 argumentos;
+2. Se qualquer um dos três argumentos não estiverem preenchidos, a função deve retornar a string:
+    Preencha todos os valores corretamente!
+3. O retorno da função deve ser a multiplicação dos 3 argumentos, somando `2` ao resultado da multiplicação.
+*/
+// var a = 2
+// var b = 3
+// var c = 4
+function multiplication(a, b, c) {
+  if ((!a || !b || !c)) {
+    var text = 'Preencha todos os valores corretamente!';
+    return text;
+  } else {
+    return a * b * c + 2
+  }
+}
+console.log(multiplication())
+console.log(multiplication(1,2,3))
 
-// // Invoque a função criada acima, passando só dois números como argumento.
-// console.log(multiplication('',3,4))
+// Invoque a função criada acima, passando só dois números como argumento.
+console.log(multiplication('',3,4))
 
-// // Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
-// // 2
+// Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
+// 2
 
-// // Agora invoque novamente a função criada acima, mas passando todos os três argumentos necessários.
-// console.log(multiplication(2,3,4))
+// Agora invoque novamente a função criada acima, mas passando todos os três argumentos necessários.
+console.log(multiplication(2,3,4))
 
-// // Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
-// // 26
+// Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
+// 26
 
 /*
 Crie uma função com as seguintes características:
