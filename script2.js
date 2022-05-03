e2bc07033c345b8b457bffa21a86331843eb1281
Uener Coelho <uenercoelho2@hotmail.com>
2022-05-02 22:08:48 -0400

    Training

--- script2.js
+++ script2.js
@@ -4,106 +4,107 @@
 
 //  Cadastro de produtos;
 
-var produto = {
-  id: null, // Number
-  descricao: null, // String
-  valor: null, // Number tem q ser positivo
-  vendedor: funcionario // iterar os funcionário pelo nome, caso contrário, de erro
-}
-
 var prompt = require("prompt-sync")();
 
 (function (prompt) {
   'use strict';
-	
+
   var condicional = true;
 
   var funcionarios = [];
 
-  // 1- função pura (só depende dela mesmo) 2 - função impura (depende outras)
-  function buscarFuncionarioPorNome(nome) {
-    // for (let funcionario of funcionarios)
-    // funcionarios.forEach(fn)
-    for (var index = 0; index < funcionários.length; index += 1) {
+  var produtos = [];
+
+	var produto = {
+    id: null, // Number
+    descricao: null, // String
+    valor: null, // Number, tem que ser positivo
+    _vendedor: buscarFuncionarioPorNome //funcionario // Iterar com os funcionários, casa contrário de erro.
+    // get vendedor() {
+    //   return this.vendedor;
+    // },
+    // set vendedor(vendedor) {
+    //   var vendedor = String.push(funcionarios);
+    // }
+  };
+
+  // 1 - Função pura (só depende dela mesmo) 2 - função impura (depende de outras)
+  function buscarFuncionarioPorNome(pessoa.nome) {
+    for ( var index = 0; index < funciconarios.length; index += 1) {
       var funcionario = funcionarios[index];
       return funcionario;
     }
-  //   funcionarios.indexOf('');
-  //   this.nome = nome;
-  // }
+  }
+
   var pessoa = {
     nome: null, // String
-    // idade: null, // Number (conversão de string para number através de função || get/set) 
     _idade: null,
     get idade() {
       return this.idade;
     },
-
     set idade(idade) {
       var idade = Number(idade);
-      if (idade <= 0) throw new Error ('Idade Digite uma idade Válida');
+      if (idade <= 0) throw new Error ('Digite uma idade Válida');
       this._idade = idade;
     },
-    // sexo: null, // M - Masculino, F - Feminino (String) (validação através de função || get/set)
     _sexo: null,
     get sexo() {
       return this.sexo;
     },
-
-    set sexo(sexo) {
+    set sexo(sexo){
       var sexo = String(sexo);
-      // if (sexo === 'M' || sexo === 'F') {
-      //   return this._sexo = sexo;
-      // }
       if (sexo !== 'M' && sexo !== 'F') throw new Error ('Digite apenas M ou F')
-
-      this._sexo = sexo
-
+      this._sexo = sexo;
     },
     cargo: null, // String
     salario: null, // Number
-    endereco: {
-      cep: null, // String
+    endereço: {
       estado: null, // String
+      cep: null // Number
     },
     departamento: {
       nome: null, // String
       responsavel: {
         nome: null, // String
-        id: Number // Number
-      },
-    },
-    
+        id: null, // Number
+      }
+    }
   };
 
   do {
-    console.log('Cadastro de Funcionário');
+    console.log('Cadastro de Funcionário e Produtos');
 
-    var menu = prompt('Informe o nome [1] Cadastrar Funcionário [2] Cadastrar Produto e [9] Sair: ');
+    var menu = prompt('Digite [1] para Cadastrar Funcionários, [2] para Cadastrar Produtos e [9] para Sair.');
 
     switch (menu) {
       case '1':
-        console.log('Cadastrar Funcionário Selecionado!');
-        pessoa.nome = prompt('Informe o nome: ');
+        console.log('Cadastro de Funcionários Selecionado!');
+        pessoa.nome = prompt('Informe o Nome: ');
         pessoa.idade = prompt('Informe a idade: ');
-        pessoa.sexo = prompt('Informe o sexo: ');
-        pessoa.cargo = prompt('Informe o cargo: ');
-        pessoa.salario = prompt('Informe o salário: ');
-        pessoa.endereco.cep = prompt('Informe o cep: ');
-        pessoa.endereco.estado = prompt('Informe o estado: ');
-        pessoa.departamento.nome = prompt('Informe o nome do departamento: ');
-        pessoa.departamento.responsavel.nome = prompt('Informe o nome do responsável pelo departamento: ');
-        pessoa.departamento.responsavel.id = prompt('Informe o id do responsável: ');
+        pessoa.sexo = prompt('Informe o Sexo: ');
+        pessoa.cargo = prompt('Informe o Cargo: ');
+        pessoa.salario = prompt('Informe o Salário: ');
+        pessoa.endereço.cep = prompt('Informe o CEP: ');
+        pessoa.endereço.estado = prompt('Informe o estado: ');
+        pessoa.departamento.nome = prompt('Informe o nome do Departamento: ');
+        pessoa.departamento.responsavel.nome = prompt('Informe o nome do Responsável pelo Departamento: ');
+        pessoa.departamento.responsavel.id = prompt('Informe o ID do Responsável pelo Departamento: ');
 
         funcionarios.push(pessoa);
 
-        console.log(funcionarios)
+        console.log(funcionarios);
 
         break;
       case '2':
-        produto.id = prompt('')
-        produto.descricao = prompt('')
-        produto.valor = prompt('')
+        produto.id = prompt('Digite o ID do produto: ')
+        produto.descricao = prompt('Digite a descrição do produto: ')
+        produto.valor = prompt('Digite o Valor do Produto: ')
+        produto.vendedor = prompt('Digite o nome do Funcionário: ')
+
+        produtos.push(produto);
+
+        console.log(produtos);
+
         break;
       case '9':
         console.log('Tchau-brigado')
@@ -114,10 +115,9 @@ var prompt = require("prompt-sync")();
         condicional = false;
         break;
     };
-    
+
   } while (condicional);
 
-  // O javascript cria PROPRIEDADES dinamicamente, SOMENTE propriedades
-	
-	console.log(pessoa);
-}(prompt));
+// O javascript cria PROPRIEDADES dinamicamente, SOMENTE propriedades
+
+}(prompt))
--- script3.js
+++ script3.js
@@ -112,3 +112,127 @@ var prompt = require("prompt-sync")();
 	
 	console.log(pessoa);
 }(prompt));
+
+//################################################################
+
+
+var produto = {
+  id: null, // Number
+  descricao: null, // String
+  valor: null, // Number tem q ser positivo
+  vendedor: buscarFuncionarioPorNome // iterar os funcionário pelo nome, caso contrário, de erro
+}
+
+var condicional = true;
+
+var funcionarios = [];
+
+// 1- função pura (só depende dela mesmo) 2 - função impura (depende outras)
+function buscarFuncionarioPorNome(nome) {
+  // for (let funcionario of funcionarios)
+  // funcionarios.forEach(fn)
+  for (var index = 0; index < funcionários.length; index += 1) {
+    var funcionario = funcionarios[index];
+    if (funcionario == ' ') throw new Erro ('Funcionário não cadastrado!')
+    return funcionario;
+  }
+//   funcionarios.indexOf('');
+//   this.nome = nome;
+// }
+var pessoa = {
+  nome: null, // String
+  _idade: null,
+  get idade() {
+    return this.idade;
+  },
+
+  set idade(idade) {
+    var idade = Number(idade);
+    if (idade <= 0) throw new Error ('Idade Digite uma idade Válida');
+    this._idade = idade;
+  },
+  _sexo: null,
+  get sexo() {
+    return this.sexo;
+  },
+
+  set sexo(sexo) {
+    var sexo = String(sexo);
+    // if (sexo === 'M' || sexo === 'F') {
+    //   return this._sexo = sexo;
+    // }
+    if (sexo !== 'M' && sexo !== 'F') throw new Error ('Digite apenas M ou F')
+
+    this._sexo = sexo
+
+  },
+  cargo: null, // String
+  salario: null, // Number
+  endereco: {
+    _cep: null, // String
+    get cep() {
+      return this.cep;
+    },
+
+    set cep(cep) {
+      var cepExp = /^\([0-9]{2}\) [0-9]{5}-[0-9]{3}$/;
+      this._cep = cep;
+    },
+
+    estado: null, // String
+  },
+  departamento: {
+    nome: null, // String
+    responsavel: {
+      nome: null, // String
+      id: Number // Number
+    },
+  },
+  
+};
+
+do {
+  console.log('Cadastro de Funcionário');
+
+  var menu = prompt('Informe o nome [1] Cadastrar Funcionário [2] Cadastrar Produto e [9] Sair: ');
+
+  switch (menu) {
+    case '1':
+      console.log('Cadastrar Funcionário Selecionado!');
+      pessoa.nome = prompt('Informe o nome: ');
+      pessoa.idade = prompt('Informe a idade: ');
+      pessoa.sexo = prompt('Informe o sexo: ');
+      pessoa.cargo = prompt('Informe o cargo: ');
+      pessoa.salario = prompt('Informe o salário: ');
+      pessoa.endereco.cep = prompt('Informe o cep: ');
+      pessoa.endereco.estado = prompt('Informe o estado: ');
+      pessoa.departamento.nome = prompt('Informe o nome do departamento: ');
+      pessoa.departamento.responsavel.nome = prompt('Informe o nome do responsável pelo departamento: ');
+      pessoa.departamento.responsavel.id = prompt('Informe o id do responsável: ');
+
+      funcionarios.push(pessoa);
+
+      console.log(funcionarios)
+
+      break;
+    case '2':
+      produto.id = prompt('Digite o ID do produto: ')
+      produto.descricao = prompt('Digite a descrição do produto: ')
+      produto.valor = prompt('Digite o Valor do Produto: ')
+      produto.vendedor = prompt('Digite o nome do Funcionário: ')
+      break;
+    case '9':
+      console.log('Tchau-brigado')
+      condicional = false;
+      break;
+    default:
+      console.log('Menu inválido!')
+      condicional = false;
+      break;
+  };
+  
+} while (condicional);
+
+// O javascript cria PROPRIEDADES dinamicamente, SOMENTE propriedades
+
+console.log(pessoa);
